d1f0904f9762740ea63bfc3a753d82279a6f4198
Alex Vukadinov <alexvuka1@gmail.com>
2023-09-02 13:25:05 +0100

    initial commit

---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+    target: ["esnext", "node18"],
+    entry: ['src/adapter.ts'],
+    format: ["esm", "cjs"],
+    sourcemap: true,
+    clean: true,
+    dts: true,
+    outDir: "dist",
+    outExtension({ format }) {
+        return {
+            js: format === "esm" ? ".mjs" : format === "iife" ? ".js" : ".cjs",
+        }
+    },
+})
