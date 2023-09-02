6fdce6b55e2b6844312ebc797f5c42b7749f39b8
Alex Vukadinov <alexvuka1@gmail.com>
2023-09-02 14:11:42 +0100

    fix: include entrypoint in tsup build

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
     target: ["esnext", "node18"],
-    entry: ['src/adapter.ts'],
+    entry: ['src/adapter.ts', 'src/entrypoint.ts'],
     format: ["esm", "cjs"],
     sourcemap: true,
     clean: true,
